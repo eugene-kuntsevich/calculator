8b86eb37d767f0b73a015779c11f57c82ab74205
Pegas7 <ifk88@mail.ru>
2016-08-31 23:07:17 +0300

    Added string ConsoleHelper.writeMessage("You select...."); in each methods class ArithmeticOperation

--- src/main/java/com/jgp7/ArithmeticOperation.java
+++ src/main/java/com/jgp7/ArithmeticOperation.java
@@ -11,26 +11,31 @@ public class ArithmeticOperation {
     private static List<Double> digits;
 
     public static double division() throws ExitException, DivisionByZeroException {
+        ConsoleHelper.writeMessage("You select division (for example: 3/2=1.5).");
         digits = ConsoleHelper.readTwoDigitsDivision();
         return digits.get(0) / digits.get(1);
     }
 
     public static double addition() throws ExitException {
+        ConsoleHelper.writeMessage("You select addition (for example: 3+2=5.0).");
         digits = ConsoleHelper.readTwoDigits();
         return digits.get(0) + digits.get(1);
     }
 
     public static double subtraction() throws ExitException {
+        ConsoleHelper.writeMessage("You select subtraction (for example: 3-2=1.0).");
         digits = ConsoleHelper.readTwoDigits();
         return digits.get(0) - digits.get(1);
     }
 
     public static double multiplication() throws ExitException {
+        ConsoleHelper.writeMessage("You select multiplication (for example: 3*2=6.0).");
         digits = ConsoleHelper.readTwoDigits();
         return digits.get(0) * digits.get(1);
     }
 
     public static double involution() throws ExitException {
+        ConsoleHelper.writeMessage("You select involution (for example: 3^2=9.0).");
         digits = ConsoleHelper.readTwoDigits();
         return Math.pow(digits.get(0), digits.get(1));
     }
