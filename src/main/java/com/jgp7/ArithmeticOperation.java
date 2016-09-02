5d1318c4065c89402003a09b9d3238e219457d4f
Pegas7 <ifk88@mail.ru>
2016-09-02 15:08:36 +0300

    Change methods class ArithmeticOperation

--- src/main/java/com/jgp7/ArithmeticOperation.java
+++ src/main/java/com/jgp7/ArithmeticOperation.java
@@ -2,6 +2,9 @@ package com.jgp7;
 
 import com.jgp7.exception.DivisionByZeroException;
 import com.jgp7.exception.ExitException;
+import com.jgp7.helpers.ConsoleMessenger;
+import com.jgp7.helpers.HelperMethods;
+
 import java.util.List;
 
 /**
@@ -11,32 +14,32 @@ public class ArithmeticOperation {
     private static List<Double> digits;
 
     public static double division() throws ExitException, DivisionByZeroException {
-        ConsoleHelper.writeMessage("You select division (for example: 3/2=1.5).");
-        digits = ConsoleHelper.readTwoDigitsDivision();
+        ConsoleMessenger.writeMessage("You select division (for example: 3/2=1.5).");
+        digits = HelperMethods.readTwoDigitsDivision();
         return digits.get(0) / digits.get(1);
     }
 
     public static double addition() throws ExitException {
-        ConsoleHelper.writeMessage("You select addition (for example: 3+2=5.0).");
-        digits = ConsoleHelper.readTwoDigits();
+        ConsoleMessenger.writeMessage("You select addition (for example: 3+2=5.0).");
+        digits = HelperMethods.readTwoDigits();
         return digits.get(0) + digits.get(1);
     }
 
     public static double subtraction() throws ExitException {
-        ConsoleHelper.writeMessage("You select subtraction (for example: 3-2=1.0).");
-        digits = ConsoleHelper.readTwoDigits();
+        ConsoleMessenger.writeMessage("You select subtraction (for example: 3-2=1.0).");
+        digits = HelperMethods.readTwoDigits();
         return digits.get(0) - digits.get(1);
     }
 
     public static double multiplication() throws ExitException {
-        ConsoleHelper.writeMessage("You select multiplication (for example: 3*2=6.0).");
-        digits = ConsoleHelper.readTwoDigits();
+        ConsoleMessenger.writeMessage("You select multiplication (for example: 3*2=6.0).");
+        digits = HelperMethods.readTwoDigits();
         return digits.get(0) * digits.get(1);
     }
 
     public static double involution() throws ExitException {
-        ConsoleHelper.writeMessage("You select involution (for example: 3^2=9.0).");
-        digits = ConsoleHelper.readTwoDigits();
+        ConsoleMessenger.writeMessage("You select involution (for example: 3^2=9.0).");
+        digits = HelperMethods.readTwoDigits();
         return Math.pow(digits.get(0), digits.get(1));
     }
 }
