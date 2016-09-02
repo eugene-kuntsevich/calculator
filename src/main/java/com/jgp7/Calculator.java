d03759e7de6b249cb399ad2edf18cdd32b71bad5
Pegas7 <ifk88@mail.ru>
2016-09-02 15:06:53 +0300

    transfer methods from Calculator in class HelperMethods

--- src/main/java/com/jgp7/Calculator.java
+++ src/main/java/com/jgp7/Calculator.java
@@ -2,19 +2,21 @@ package com.jgp7;
 
 import com.jgp7.exception.DivisionByZeroException;
 import com.jgp7.exception.ExitException;
+import com.jgp7.helpers.ConsoleMessenger;
+import com.jgp7.helpers.HelperMethods;
 
 /**
  * Created by Evgeni Kuntsevich on 02.09.2016.
  */
 public class Calculator {
     public void run() {
-        ConsoleHelper.welcomeMessage();
+        ConsoleMessenger.welcomeMessage();
 
         while (true) {
             try {
-                int numberOfOperation = getNumber();
+                int numberOfOperation = HelperMethods.getNumber();
                 TypesArithmeticOperation operation =
-                        getOperation(numberOfOperation);
+                        HelperMethods.getOperation(numberOfOperation);
 
                 double resultOperation;
 
@@ -35,32 +37,24 @@ public class Calculator {
                         resultOperation = ArithmeticOperation.involution();
                         break;
                     default:
-                        ConsoleHelper.writeMessage("You entered no number of operation. Please, try again.");
+                        ConsoleMessenger.writeMessage("You entered no number of operation. Please, try again.");
                         continue;
                 }
 
-                ConsoleHelper.writeMessage("Result of operation = " + String.valueOf(resultOperation));
+                ConsoleMessenger.writeMessage("Result of operation = " + String.valueOf(resultOperation));
 
             } catch (ExitException e) {
-                ConsoleHelper.writeMessage("Good bye!");
+                ConsoleMessenger.writeMessage("Good bye!");
                 break;
             } catch (DivisionByZeroException e) {
-                ConsoleHelper.divisionByZeroExceptionMessage();
+                ConsoleMessenger.divisionByZeroExceptionMessage();
                 continue;
             } catch (NumberFormatException e) {
-                ConsoleHelper.writeMessage("You entered no digit or \"exit\". Please, enter digit of operation or \"exit\" and press \"Enter\" again.");
+                ConsoleMessenger.writeMessage("You entered no digit or \"exit\". Please, enter digit of operation or \"exit\" and press \"Enter\" again.");
                 continue;
             }
 
-            ConsoleHelper.executeNextOperationMessage();
+            ConsoleMessenger.executeNextOperationMessage();
         }
-    }
-
-    private static int getNumber() throws ExitException {
-        return Integer.parseInt(ConsoleHelper.readString());
-    }
-
-    private static TypesArithmeticOperation getOperation(int i) {
-        return TypesArithmeticOperation.values()[i - 1];
     }
 }
