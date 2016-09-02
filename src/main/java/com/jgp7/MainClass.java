9f43da8a403a8fdb78e11305304c8452b5abcdae
Pegas7 <ifk88@mail.ru>
2016-09-02 14:33:16 +0300

    Transfer logic from MainClass in class Calculator

--- src/main/java/com/jgp7/MainClass.java
+++ src/main/java/com/jgp7/MainClass.java
@@ -1,7 +1,5 @@
 package com.jgp7;
 
-import com.jgp7.exception.DivisionByZeroException;
-import com.jgp7.exception.ExitException;
 import java.io.IOException;
 
 /**
@@ -9,42 +7,6 @@ import java.io.IOException;
  */
 public class MainClass {
     public static void main(String[] args) throws IOException {
-        ConsoleHelper.welcomeMessage();
-
-        while (true) {
-            try {
-                int numberOfOperation = Integer.parseInt(ConsoleHelper.readString());
-                double resultOperation = 0;
-
-                if (numberOfOperation == TypesArithmeticOperation.ADDITION.ordinal() + 1)
-                    resultOperation = ArithmeticOperation.addition();
-                else if (numberOfOperation == TypesArithmeticOperation.SUBTRACTION.ordinal() + 1)
-                    resultOperation = ArithmeticOperation.subtraction();
-                else if (numberOfOperation == TypesArithmeticOperation.MULTIPLICATION.ordinal() + 1)
-                    resultOperation = ArithmeticOperation.multiplication();
-                else if (numberOfOperation == TypesArithmeticOperation.DIVISION.ordinal() + 1)
-                    resultOperation = ArithmeticOperation.division();
-                else if (numberOfOperation == TypesArithmeticOperation.INVOLUTION.ordinal() + 1)
-                    resultOperation = ArithmeticOperation.involution();
-                else {
-                    ConsoleHelper.writeMessage("You entered no number of operation. Please, try again.");
-                    continue;
-                }
-
-                ConsoleHelper.writeMessage("Result of operation = " + String.valueOf(resultOperation));
-
-            } catch (ExitException e) {
-                ConsoleHelper.writeMessage("Good bye!");
-                break;
-            } catch (DivisionByZeroException e) {
-                ConsoleHelper.divisionByZeroExceptionMessage();
-                continue;
-            } catch (NumberFormatException e) {
-                ConsoleHelper.writeMessage("You entered no digit or \"exit\". Please, enter digit of operation or \"exit\" and press \"Enter\" again.");
-                continue;
-            }
-
-            ConsoleHelper.executeNextOperationMessage();
-        }
+        new Calculator().run();
     }
 }
