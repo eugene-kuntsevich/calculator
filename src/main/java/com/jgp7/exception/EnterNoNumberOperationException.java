e8df1d2262f70d55d22246f7d7dc2e3b03b210b4
Pegas7 <ifk88@mail.ru>
2016-09-02 18:02:15 +0300

    Added exception EnterNoNumberOperationException

--- src/main/java/com/jgp7/exception/EnterNoNumberOperationException.java
+++ src/main/java/com/jgp7/exception/EnterNoNumberOperationException.java
@@ -3,5 +3,5 @@ package com.jgp7.exception;
 /**
  * Created by Evgeni Kuntsevich on 02.09.2016.
  */
-public class enterNoNumberOperationException {
+public class EnterNoNumberOperationException extends Exception {
 }
