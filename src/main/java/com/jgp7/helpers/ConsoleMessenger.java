a895fc2f59c28851ec884c854ce0a70476d05edf
Pegas7 <ifk88@mail.ru>
2016-09-02 15:07:56 +0300

    Trsansfer methods from class Console Messenger in class HelperMethods

--- src/main/java/com/jgp7/helpers/ConsoleMessenger.java
+++ src/main/java/com/jgp7/helpers/ConsoleMessenger.java
@@ -1,79 +1,11 @@
-package com.jgp7;
-
-import com.jgp7.exception.DivisionByZeroException;
-import com.jgp7.exception.ExitException;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
+package com.jgp7.helpers;
 
 /**
  * Created by Evgeni Kuntsevich on 30.08.2016.
  */
-public class ConsoleHelper {
-    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+public class ConsoleMessenger {
     private static String operationItems = "\n1. addition (for example: 3+2=5.0); \n2. subtraction (for example: 3-2=1.0);\n3. multiplication (for example: 3*2=6.0); \n4. division (for example: 3/2=1.5);\n5. involution (for example: 3^2=9.0).";
-
-    public static String readString() throws ExitException {
-        String inputString = "";
-
-        try {
-            inputString = reader.readLine();
-            if (inputString.equalsIgnoreCase("exit")) throw new ExitException();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return inputString;
-    }
-
-    public static List<Double> readTwoDigits() throws ExitException {
-        List<Double> digits = new LinkedList();
-
-        ConsoleHelper.writeMessage("Enter first digit and press \"Enter\" after entered data.");
-
-        while (true) {
-            try {
-                String inputString = ConsoleHelper.readString();
-                digits.add(Double.valueOf(inputString));
-                if (digits.size() == 2) break;
-                ConsoleHelper.writeMessage("Enter second digit and press \"Enter\" after entered data.");
-            } catch (NumberFormatException e) {
-                ConsoleHelper.writeMessage("Error! You entered no digit. Try to repeat all again. Enter first digit and press \"Enter\" after entered data.");
-                digits.clear();
-            }
-        }
-
-        return digits;
-    }
-
-    public static List<Double> readTwoDigitsDivision() throws ExitException, DivisionByZeroException {
-        List<Double> digits = new LinkedList();
-
-        while (true) {
-            try {
-                ConsoleHelper.writeMessage("Enter first digit and press \"Enter\" after entered data.");
-                String inputString = ConsoleHelper.readString();
-                digits.add(Double.valueOf(inputString));
-
-                ConsoleHelper.writeMessage("Enter second digit and press \"Enter\" after entered data.");
-                inputString = ConsoleHelper.readString();
-
-                if (Double.valueOf(inputString) == 0) throw new DivisionByZeroException();
-                else {
-                    digits.add(Double.valueOf(inputString));
-                    break;
-                }
-            } catch (NumberFormatException e) {
-                ConsoleHelper.writeMessage("Error! You entered no digit. Try to repeat all again.");
-                digits.clear();
-            }
-        }
-
-        return digits;
-    }
-
+    
     public static void writeMessage(String message) {
         System.out.println(message);
     }
